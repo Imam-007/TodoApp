484229dd8b24d99be5a59b6169e9bfff2429b360
Imam-007 <imimam8409@gmail.com>
2024-11-07 14:16:57 +0530

    added code for marking completed task

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,10 +5,22 @@ export default function TodoItem({ item, todos, setTodos }) {
     console.log("clicked item is ", item);
     setTodos(todos.filter((todo) => todo !== item));
   }
+
+  function handleClick(name) {
+    const newTodo = todos.map((todo) =>
+      todo.name === name ? { ...todo, done: !todo.done } : todo
+    );
+    setTodos(newTodo);
+  }
+
+  const lineThrough = item.done ? styles.completed : "";
+
   return (
     <div className={styles.item}>
       <div className={styles.itemName}>
-        {item}{" "}
+        <span className={lineThrough} onClick={() => handleClick(item.name)}>
+          {item.name}
+        </span>
         <span>
           <button
             onClick={() => handleDelete(item)}
