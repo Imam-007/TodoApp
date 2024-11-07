4e16a6b154309378f3d47d5ab333af3b4c5c2187
Imam-007 <imimam8409@gmail.com>
2024-11-07 11:25:11 +0530

    added delete button and also provide it's functionality

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,9 +1,23 @@
 import styles from "./todoitem.module.css";
 
-export default function TodoItem(props) {
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    console.log("clicked item is ", item);
+    setTodos(todos.filter((todo) => todo !== item));
+  }
   return (
     <div className={styles.item}>
-      <div className={styles.itemName}>{props.item}</div>
+      <div className={styles.itemName}>
+        {item}{" "}
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteBtn}
+          >
+            X
+          </button>
+        </span>
+      </div>
       <hr className={styles.line} />
     </div>
   );
